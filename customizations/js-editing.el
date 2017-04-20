dea93768118a0a8a1062965e42b3aaf2bef43605
Mitchell Hillman <mhillman@roomkey.com>
2017-04-20 15:48:31 -0400

    add auto linting

--- customizations/js-editing.el
+++ customizations/js-editing.el
@@ -47,4 +47,15 @@
       ad-do-it)
     ad-do-it))
 
-
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
