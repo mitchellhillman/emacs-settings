1734c858d56329dd02194961818dbd948d76e35e
Mitchell Hillman <mhillman@roomkey.com>
2017-04-21 10:50:29 -0400

    stop tracking packages folder, and update stuff

--- elpa/folding-20140401.3/folding-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "folding" "20140401.3" "A folding-editor-like minor mode." 'nil :keywords '("tools"))
--- init.el
+++ init.el
@@ -74,7 +74,14 @@
     tagedit
 
     ;; git integration
-    magit))
+    magit
+
+    multiple-cursors
+    flycheck
+    js2-mode
+    json-mode
+    pug-mode
+    web-mode))
 
 ;; On OS X, an Emacs instance started from the graphical user
 ;; interface will have a different environment than a shell in a
