7356adcd1fb198aff10684aa4beb65bef2168619
Mitchell Hillman <mhillman@roomkey.com>
2017-06-08 11:06:49 -0400

    web-mode fixed for .js files

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -7,18 +7,19 @@
 (defun my-web-mode-hook ()
   "Hooks for Web mode. Adjust indents"
   ;;; http://web-mode.org/
+  (web-mode-set-content-type "jsx")
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2))
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
-;; for better jsx syntax-highlighting in web-mode
-;; - courtesy of Patrick @halbtuerke
-(defadvice web-mode-highlight-part (around tweak-jsx activate)
-  (if (equal web-mode-content-type "jsx")
-    (let ((web-mode-enable-part-face nil))
-      ad-do-it)
-    ad-do-it))
+;; ;; for better jsx syntax-highlighting in web-mode
+;; ;; - courtesy of Patrick @halbtuerke
+;; (defadvice web-mode-highlight-part (around tweak-jsx activate)
+;;   (if (equal web-mode-content-type "jsx")
+;;     (let ((web-mode-enable-part-face nil))
+;;       ad-do-it)
+;;     ad-do-it))
 
 
 ;; Flycheck
